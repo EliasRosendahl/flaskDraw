559bd5fc791c56ee0305a86a34d31052cf101551
eliasrosendahl <jensen.eliasrosendahl@gmail.com>
2018-10-05 21:53:40 +0200

    canvas.js now store the difference in pixels after a drawing action

--- static/canvas.js
+++ static/canvas.js
@@ -4,7 +4,10 @@ var c = document.getElementById("canvasObj");
 var context = c.getContext("2d");
 
 c.addEventListener("click", function(){
-    console.log("click");
+    //store original imgData to compare the difference
+    //TODO : should take previous imgDataNew 
+    var imgDataOld = context.getImageData(0, 0, 720, 480);
+
     var x = event.x;
     var y = event.y;
 
@@ -12,6 +15,13 @@ c.addEventListener("click", function(){
     y -= c.offsetTop;
 
     context.fillRect(x, y, 10, 10);
+    
+    var imgDataNew = context.getImageData(0, 0, 720, 480);
+    var imgDataDiff = {data: {}};
 
-    console.log(x + y);
+    for (let index = 0; index < imgDataNew.data.length; index++) {
+        if(imgDataOld.data[index] !== imgDataNew.data[index]){
+            imgDataDiff.data[index] = imgDataNew.data[index];
+        }
+    }
 });
